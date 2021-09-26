63f39a786707c7f84d65b728e4b860bcce503201
p22jiacono <90051927+p22jiacono@users.noreply.github.com>
2021-09-26 17:21:41 -0400

    hw2-6-2

---
+++ hw2-6-2.py
@@ -0,0 +1,14 @@
+#Author: JTI 9/23/21
+
+height=input("Height of cylinder: ")
+radius=input("Radius of cylinder: ")
+volume=((3.14) * (int(radius) ** 2) * (int(height))
+surface= ((2)*(3.14)*int(radius))*(int(radius)+(int(height)))
+print(volume,surface)
+
+
+
+
+
+
+
